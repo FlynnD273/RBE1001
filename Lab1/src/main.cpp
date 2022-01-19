b1ee74ded00abd98fb1060eeca0a82b18ad0eb25
Flynn Duniho <flynn.duniho@bellevuecollege.edu>
2022-01-19 00:08:18 -0500

    Add starter file in preparation for lab 1

---
+++ Lab1/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Flynn                                            */
+/*    Created:      Wed Jan 19 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
