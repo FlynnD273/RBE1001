7e9d3acc8700556329613c6df1fbe3fb469a112c
zakaplan <zakaplan88@gmail.com>
2022-01-26 09:51:53 -0500

    Added Lab 2

---
+++ RBE1001Lab2-2022-01-26T14-51-19/src/main.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\zakap                                            */
+/*    Created:      Wed Jan 26 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// MotorL               motor         10              
+// MotorR               motor         1               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  double voltage = 3;
+  
+  
+}
