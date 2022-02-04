e3df581d286a8cbd39c561885fae5c3ad98057ad
zakaplan <zakaplan88@gmail.com>
2022-02-04 14:09:27 -0500

    Line Following Code
    No, it won't be useful :P

--- Lab3/LineFollow/src/main.cpp
+++ Lab3/LineFollow/src/main.cpp
@@ -20,9 +20,8 @@
 #include "robot.h"
 
 using namespace vex;
-
-const double K_P { 0.1 };
-const double BASE_SPEED { -0.4 };
+const double K_P { 0.3 };
+const double BASE_SPEED { -9};
 const int THRESHOLD { 25 };
 
 int main() {
@@ -35,14 +34,21 @@ int main() {
   //   task::sleep(100);
   // }
   task::sleep(500);
+  MotorL.spin(forward);
+  MotorR.spin(forward);
   while (LineTrackerL.reflectivity() > THRESHOLD || LineTrackerR.reflectivity() > THRESHOLD)
   {
-    MotorR.spin(forward, LineTrackerR.reflectivity() * K_P + BASE_SPEED, volt);
-    MotorL.spin(forward, LineTrackerL.reflectivity() * K_P + BASE_SPEED, volt);
+    /*MotorR.spin(forward, LineTrackerR.reflectivity() * K_P + BASE_SPEED, volt);
+    MotorL.spin(forward, LineTrackerL.reflectivity() * K_P + BASE_SPEED, volt);*/
+    MotorL.setVelocity(LineTrackerL.reflectivity() * K_P + BASE_SPEED, rpm);
+    MotorR.setVelocity(LineTrackerR.reflectivity() * K_P + BASE_SPEED, rpm);
     printf("Left: %lu\n", LineTrackerL.reflectivity());
     printf("Right: %lu\n", LineTrackerR.reflectivity());
+    printf("RPM: %lu\n", LineTrackerR.reflectivity() * K_P);
     task::sleep(50);
   }
   turn(90, 300);
   printf("Done");
+  task::sleep(300);
+  return 0;
 }
