c9dc4230a3f2083aee4da0dff2b950f57735a6fa
zakaplan <zakaplan88@gmail.com>
2022-02-09 08:26:55 -0500

    Updated lab 4 code
    Added a vision sensor

--- Team-13-Lab-4/include/robot-config.h
+++ Team-13-Lab-4/include/robot-config.h
@@ -2,9 +2,21 @@ using namespace vex;
 
 extern brain Brain;
 
+using signature = vision::signature;
+
+// VEXcode devices
+extern signature Vision17__SIG_1;
+extern signature Vision17__SIG_2;
+extern signature Vision17__SIG_3;
+extern signature Vision17__SIG_4;
+extern signature Vision17__SIG_5;
+extern signature Vision17__SIG_6;
+extern signature Vision17__SIG_7;
+extern vision Vision17;
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void);
+void  vexcodeInit( void );
--- Team-13-Lab-4/src/main.cpp
+++ Team-13-Lab-4/src/main.cpp
@@ -8,6 +8,9 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Vision17             vision        17              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -15,7 +18,6 @@
 using namespace vex;
 
 int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   
 }
