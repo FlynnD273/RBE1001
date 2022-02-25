2ab069999c0758af40c40aae372277e3cb7054f2
Flynn Duniho <flynn.duniho@bellevuecollege.edu>
2022-02-25 13:46:01 -0500

    Testing calibration for sticky notes instead of shiny tape

--- Autonomous/Autonomous-2022-02-25T17-46-35/src/main.cpp
+++ Autonomous/Autonomous-2022-02-25T17-46-35/src/main.cpp
@@ -10,6 +10,18 @@
 // RangeR               sonar         G, H            
 // VisionF              vision        15              
 // ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// MotorsR              motor_group   3, 5            
+// MotorsL              motor_group   9, 10           
+// MotorsB              motor_group   11, 20          
+// MotorC               motor         4               
+// LineR                line          B               
+// LineL                line          A               
+// RangeR               sonar         G, H            
+// VisionF              vision        15              
+// ---- END VEXCODE CONFIGURED DEVICES ----
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
